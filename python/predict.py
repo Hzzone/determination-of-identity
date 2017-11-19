938914053b54c075f8166c4f59a0447b24ff1678
Hzzone <Hzzone@outlook.com>
2017-11-19 19:58:57 +0800

    train end

--- python/predict.py
+++ python/predict.py
@@ -6,20 +6,18 @@ import os
 caffe_root = '/home/hzzone/determination-of-identity/C3D/C3D-v1.1'  # this file is expected to be in {caffe_root}/examples/siamese
 import sys
 sys.path.insert(0, os.path.join(caffe_root, 'python'))
-# import caffe
+import caffe
 import pylab
-import matplotlib.pyplot as plt
-import dicom
-import test
 import distance
 
 # source: Test dataset
-def ordinary_predict_dataset(source, caffemodel, deploy_file, LAST_LAYER_NAME, save_features_file, gpu_mode=True):
+def ordinary_predict_dataset(source, caffemodel, deploy_file, LAST_LAYER_NAME, save_features_file, gpu_mode=True, is_3d=True):
     if gpu_mode:
         caffe.set_mode_gpu()
     else:
         caffe.set_mode_cpu()
     net = caffe.Net(deploy_file, caffemodel, caffe.TEST)
+    content = ""
     for each_sample in os.listdir(source):
         p1 = os.path.join(source, each_sample)
         print(p1)
@@ -28,16 +26,19 @@ def ordinary_predict_dataset(source, caffemodel, deploy_file, LAST_LAYER_NAME, s
         study_date = im[2]
         im = im[0]
         # im = im[np.newaxis, :]
+        if is_3d:
+	        im = im[np.newaxis, :]
         net.blobs['data'].data[...] = im
         output = net.forward()
         features = output[LAST_LAYER_NAME]
-        with open(save_features_file, "a") as f:
-            f.write("%s %s %s\n" % (patient_id, study_date, " ".join(map(str, features[0].tolist()))))
+        content += "%s %s %s\n" % (patient_id, study_date, " ".join(map(str, features[0].tolist())))
+	with open(save_features_file, "w") as f:
+		f.write(content)
 
-def write_model_dir_features(caffemodel_source_dir, target_save_path, test_data_source, deploy_file, LAST_LAYER_NAME):
+def write_model_dir_features(caffemodel_source_dir, target_save_path, test_data_source, deploy_file, LAST_LAYER_NAME, is_3d=True):
     for file_name in os.listdir(caffemodel_source_dir):
         path = os.path.join(caffemodel_source_dir, file_name)
-        ordinary_predict_dataset(test_data_source, caffemodel=path, deploy_file=deploy_file, LAST_LAYER_NAME=LAST_LAYER_NAME, save_features_file=os.path.join(target_save_path, file_name+"_features.txt"))
+        ordinary_predict_dataset(test_data_source, caffemodel=path, deploy_file=deploy_file, LAST_LAYER_NAME=LAST_LAYER_NAME, save_features_file=os.path.join(target_save_path, file_name+"_features.txt"), is_3d=is_3d)
 
 def read_file_and_output_accuracy(features_file):
     with open(features_file) as f:
@@ -53,7 +54,7 @@ def read_file_and_output_accuracy(features_file):
         _same = f.readlines()
     _same = [x.split(" ") for x in _same]
     _diff = [x.split(" ") for x in _diff][:len(_same)]
-    x_values = pylab.arange(0.9999999, 1.0, 0.0000000000000001)
+    x_values = pylab.arange(0.99999999, 1.0, 0.00000000001)
     max_accuracy = 0
     max_accuracy_threshold = 0
     same_distance = []
@@ -62,6 +63,8 @@ def read_file_and_output_accuracy(features_file):
         same_distance.append(distance.cosine_distnace(samples[r[0]], samples[r[1]]))
     for r in _diff:
         diff_distance.append(distance.cosine_distnace(samples[r[0]], samples[r[1]]))
+	# print same_distance
+	# print diff_distance
     total = len(_same)+len(_diff)
     same_distance = np.array(same_distance)
     diff_distance = np.array(diff_distance)
@@ -136,10 +139,15 @@ def write(features):
 			f.write("%s %s %s %s\n" % (s[0], s[1], s[2], s[3]))
 
 if __name__ == "__main__":
-	write("../ct-test/siamese/siamese_features/c3d_lenet_iter_2400.caffemodel_features.txt")
-    # write_model_dir_features("/home/hzzone/determination-of-identity/ct-test/siamese/model", "/home/hzzone/determination-of-identity/ct-test/siamese/siamese_features", test_data_source="/home/hzzone/1tb/id-data/test", deploy_file="/home/hzzone/determination-of-identity/ct-test/siamese/3d_siamese_deploy.prototxt", LAST_LAYER_NAME="fc8")
-    # for file_name in os.listdir("../ct-test/siamese/siamese_features"):
-    #     read_file_and_output_accuracy(os.path.join("../ct-test/siamese/siamese_features", file_name))
-    # x1("./temp.txt")
-    # read__file_and_output_accuracy("./temp.txt", "/home/hzzone/determination-of-identity/python/features/lenet_siamese_train_iter_1000.caffemodel_features.txt", "1.jpg")
+	# write("../ct-test/siamese/siamese_features/c3d_lenet_iter_2400.caffemodel_features.txt")
+	# write_model_dir_features("/home/hzzone/1tb/id-model/classification/3d/3d_alexnet", "/home/hzzone/determination-of-identity/ct-test/classification/3d/3d_alexnet/features", test_data_source="/home/hzzone/1tb/id-data/test", deploy_file="/home/hzzone/determination-of-identity/ct-test/classification/3d/3d_alexnet/deploy.prototxt", LAST_LAYER_NAME="fc7")
+	# write_model_dir_features("/home/hzzone/1tb/id-model/classification/3d/3d_caffenet", "/home/hzzone/determination-of-identity/ct-test/classification/3d/3d_caffenet/features", test_data_source="/home/hzzone/1tb/id-data/test", deploy_file="/home/hzzone/determination-of-identity/ct-test/classification/3d/3d_caffenet/deploy.prototxt", LAST_LAYER_NAME="fc7")
+	# write_model_dir_features("/home/hzzone/1tb/id-model/siamese/3d/3d_alexnet", "/home/hzzone/determination-of-identity/ct-test/siamese/3d/3d_alexnet/features", test_data_source="/home/hzzone/1tb/id-data/test", deploy_file="/home/hzzone/determination-of-identity/ct-test/siamese/3d/3d_alexnet/deploy.prototxt", LAST_LAYER_NAME="fc8")
+	# write_model_dir_features("/home/hzzone/1tb/id-model/siamese/3d/3d_caffenet", "/home/hzzone/determination-of-identity/ct-test/siamese/3d/3d_caffenet/features", test_data_source="/home/hzzone/1tb/id-data/test", deploy_file="/home/hzzone/determination-of-identity/ct-test/siamese/3d/3d_caffenet/deploy.prototxt", LAST_LAYER_NAME="fc8")
+	# write_model_dir_features("/home/hzzone/1tb/id-model/classification/2d/2d_alexnet", "/home/hzzone/determination-of-identity/ct-test/classification/2d/2d_alexnet/features", test_data_source="/home/hzzone/1tb/id-data/test", deploy_file="/home/hzzone/determination-of-identity/ct-test/classification/2d/2d_alexnet/deploy.prototxt", LAST_LAYER_NAME="fc7", is_3d=False)
+	# write_model_dir_features("/home/hzzone/1tb/id-model/classification/2d/2d_caffenet", "/home/hzzone/determination-of-identity/ct-test/classification/2d/2d_caffenet/features", test_data_source="/home/hzzone/1tb/id-data/test", deploy_file="/home/hzzone/determination-of-identity/ct-test/classification/2d/2d_caffenet/deploy.prototxt", LAST_LAYER_NAME="fc7", is_3d=False)
+	# write_model_dir_features("/home/hzzone/1tb/id-model/siamese/2d/2d_alexnet", "/home/hzzone/determination-of-identity/ct-test/siamese/2d/2d_alexnet/features", test_data_source="/home/hzzone/1tb/id-data/test", deploy_file="/home/hzzone/determination-of-identity/ct-test/siamese/2d/2d_alexnet/deploy.prototxt", LAST_LAYER_NAME="fc8", is_3d=False)
+	# write_model_dir_features("/home/hzzone/1tb/id-model/siamese/2d/2d_caffenet", "/home/hzzone/determination-of-identity/ct-test/siamese/2d/2d_caffenet/features", test_data_source="/home/hzzone/1tb/id-data/test", deploy_file="/home/hzzone/determination-of-identity/ct-test/siamese/2d/2d_caffenet/deploy.prototxt", LAST_LAYER_NAME="fc8", is_3d=False)
+	for file_name in os.listdir("/home/hzzone/determination-of-identity/ct-test/classification/3d/3d_alexnet/features"):
+		read_file_and_output_accuracy(os.path.join("/home/hzzone/determination-of-identity/ct-test/classification/3d/3d_alexnet/features", file_name))
 
